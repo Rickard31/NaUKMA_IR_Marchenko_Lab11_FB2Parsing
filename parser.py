ac9d2edf48fca4c8372d4ada8e88d219da1a37d8
Rickard31 <vitalka.marchenko@gmail.com>
2018-04-08 23:41:32 +0300

    Initialised files

--- parser.py
+++ parser.py
@@ -1,1 +1,4 @@
-from xml.etree import ElementTree
+#I can't get it done by deadline
+#When I finish it, I will commit it to github.com/Rickard31/NaUKMA_IR_Marchenko_Lab11_FB2Parsing
+
+#github.com/Rickard31/NaUKMA_IR_Marchenko_Lab11_FB2Parsing
